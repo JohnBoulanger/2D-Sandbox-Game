b0b65d5999ef74beff336aaa7203afcf48de6e52
JohnBoulanger <john03yyc@gmail.com>
2026-01-08 23:21:25 -0500

    linked tgui library and added a test button in the middle of the screen

--- src/ui/UI.cpp
+++ src/ui/UI.cpp
@@ -1,5 +1,7 @@
 #include <ui/UI.h>
 #include <config/TileConstants.h>
+#include <TGUI/TGUI.hpp>
+#include <TGUI/Backend/SFML-Graphics.hpp>
 
 UI::UI()
 {
@@ -20,4 +22,15 @@ void UI::draw(sf::RenderWindow& window, sf::Vector2f mousePos)
 {
     healthBar.draw(window);
     inventory.draw(window);
+    tgui::Gui gui(window);
+    auto testButton = tgui::Button::create("Test");
+    testButton->setSize({200, 60});
+    testButton->setPosition({"50% - 100", "40%"});
+
+    testButton->onPress([&] {
+        std::cout << "Play pressed\n";
+    });
+
+    gui.add(testButton);
+    gui.draw();
 }
